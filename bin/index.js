45f870bcefeb9eb4af3ae0946fca9af3eb31a912
simov <simeonvelichkov@gmail.com>
2018-04-23 16:06:39 +0300

    Initial commit

---
+++ bin/index.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+var argv = require('minimist')(process.argv.slice(2))
+
+if (argv.help) {
+  console.log(`
+    --config  /path/to/config.json
+    --events  /path/to/events.json
+    --env     environment
+  `)
+  process.exit()
+}
+
+var env = argv.env || process.env.NODE_ENV || 'development'
+var config = require(argv.config)[env]
+var db = require(argv.events)
+
+var puppeteer = require('puppeteer')
+var options = {
+  // headless: false,
+  // slowMo: 300,
+}
+
+var lib = {
+  dom: require('../lib/dom'),
+  html: require('../lib/html'),
+  db: require('../lib/db'),
+}
+
+puppeteer.launch(options).then((browser) => {
+  setInterval(() =>
+    lib.dom/*html*/.sync({browser, config, db})
+      .then((events) => lib.db.sync({events, db, fpath: argv.events}))
+      .catch(console.error)
+    , 1000 * 60 * config.interval)
+  })
+  .catch(console.error)
